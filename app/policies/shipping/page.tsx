8c0e500074659604644c138a01cee25bab2a946e
Adrian Tobi <talktotobi.a@gmail.com>
2024-07-12 01:19:08 +0100

    shipping

---
+++ app/policies/shipping/page.tsx
@@ -0,0 +1,10 @@
+export default function Shipping() {
+  return (
+    <main>
+      <h1>Shipping</h1>
+      <p>
+        We ship to the UK and Ireland. Shipping is free on all orders over £50.
+      </p>
+    </main>
+  );
+}
